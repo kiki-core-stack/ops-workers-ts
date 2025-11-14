d1d454559ed0cd05a9b00c21e09fe0ed8cc0272a
kiki-kanri <a470666@gmail.com>
2025-11-14 23:57:33 +0800

    chore: update base files and remove unused files

--- tmux-run-dev.sh
+++ tmux-run-dev.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPTS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 cd "${SCRIPTS_DIR}"
 
-SESSION_NAME='bun-project'
+SESSION_NAME='kiki-core-stack-ops-workers-ts'
 
 if ! tmux ls | grep -q "^${SESSION_NAME}:"; then
     tmux new-session -ds "${SESSION_NAME}"
